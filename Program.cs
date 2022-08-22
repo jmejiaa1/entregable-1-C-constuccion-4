d5ab390f70e4ed177f39843f0fdfe317deaf439d
Sebastian Rojas <88469107+Petulio14@users.noreply.github.com>
2022-08-21 22:09:29 -0500

    punto 1
    se agrega el punto 1 del taller

--- Program.cs
+++ Program.cs
@@ -32,4 +32,43 @@ descuento=(entrada*3)/100;
 
 //juan diego mejia 
  
-  
+
+// punto 1 taller
+//Sebastian Rojas Gomez
+
+Console.WriteLine("ingrese los valores del primer semestrales del año");
+double valor1 = double.Parse(Console.ReadLine());
+Console.WriteLine("ingrese los valores del segundo semestrales del año");
+double valor2 = double.Parse(Console.ReadLine());
+
+if ((valor1+valor2)<10000){
+    Console.WriteLine("El total de su renta anual es: $"+(valor1+valor2));
+    Console.WriteLine("El total de renta apagar es: $"+ (valor1+valor2)*1.05);
+    Console.WriteLine("El impuesto aplicado es 5%" );
+    Environment.Exit(0);
+} 
+if ((valor1+valor2)>=10000&&(valor1+valor2)<20000){
+    Console.WriteLine("El total de su renta anual es: $"+(valor1+valor2));
+    Console.WriteLine("El total de renta apagar es: $"+ (valor1+valor2)*1.1);
+    Console.WriteLine("El impuesto aplicado es 10%" );
+    Environment.Exit(0);
+} 
+if ((valor1+valor2)>=20000&&(valor1+valor2)<35000){
+    Console.WriteLine("El total de su renta anual es: $"+(valor1+valor2));
+    Console.WriteLine("El total de renta apagar es: $"+ (valor1+valor2)*1.15);
+    Console.WriteLine("El impuesto aplicado es 15%" );
+    Environment.Exit(0);
+} 
+if ((valor1+valor2)>=35000&&(valor1+valor2)<45000){
+    Console.WriteLine("El total de su renta anual es: $"+(valor1+valor2));
+    Console.WriteLine("El total de renta apagar es: $"+ (valor1+valor2)*1.2);
+    Console.WriteLine("El impuesto aplicado es 20%" );
+    Environment.Exit(0);
+} 
+if ((valor1+valor2)>=45000){
+    Console.WriteLine("El total de su renta anual es: $"+(valor1+valor2));
+    Console.WriteLine("El total de renta apagar es: $"+ (valor1+valor2)*1.3);
+    Console.WriteLine("El impuesto aplicado es 30%" );
+    Environment.Exit(0);
+} 
+
